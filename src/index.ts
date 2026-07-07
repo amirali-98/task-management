a65d75b9064baae3bc4329c8f8669f6e0eef9490
amirali-98 <amiralimansouri98@outlook.com>
2026-07-07 14:56:21 +0330

    add global middleware to app file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import moragan from 'morgan';
 
 const app = express();
+
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(moragan('dev'));
+}
 
 export default app;
