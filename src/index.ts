2bddd0c573a158656c4ebce006a43fdbb0dd1333
amirali-98 <amiralimansouri98@outlook.com>
2026-07-08 19:06:42 +0330

    implement user routes and connect to app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import moragan from 'morgan';
 
+import userRoutes from './routes/user.route';
+
 const app = express();
 
 app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(moragan('dev'));
 }
+
+app.use('/api/v1/', userRoutes);
 
 export default app;
