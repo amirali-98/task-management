f73967d1c474ea734be8d55cb77642035aa628d4
amirali-98 <amiralimansouri98@outlook.com>
2026-07-10 19:34:12 +0330

    convert all errors to AppError custom error class

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,20 +1,57 @@
 import { NextFunction, Request, Response } from 'express';
+import { Error as MongooseError } from 'mongoose';
+import jwt from 'jsonwebtoken';
+
 import AppError from '../utils/appError.util';
 import { sendDevError, sendProdError } from '../utils/sendError.util';
+import {
+  handleCastErrorDB,
+  handleDuplicateFieldsDB,
+  handleJWTError,
+  handleJWTExpiredError,
+  handleValidationErrorDB,
+} from '../utils/errorHandlers.util';
+
+type MongoDuplicateKeyError = {
+  code?: number;
+  keyValue?: Record<string, unknown>;
+  message?: string;
+};
 
 const globalErrorHandler = (
-  err: AppError,
+  err: unknown,
   _: Request,
   res: Response,
   __: NextFunction,
 ) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+  console.dir(err, { depth: null });
+  let error: AppError;
+
+  if (err instanceof AppError) {
+    error = err;
+  } else if (err instanceof MongooseError.CastError) {
+    error = handleCastErrorDB(err);
+  } else if (err instanceof MongooseError.ValidationError) {
+    error = handleValidationErrorDB(err);
+  } else if (err instanceof jwt.TokenExpiredError) {
+    error = handleJWTExpiredError(err);
+  } else if (err instanceof jwt.JsonWebTokenError) {
+    error = handleJWTError(err);
+  } else if (
+    typeof err === 'object' &&
+    err !== null &&
+    'code' in err &&
+    err.code === 11000
+  ) {
+    error = handleDuplicateFieldsDB(err as MongoDuplicateKeyError);
+  } else {
+    error = new AppError('Something went wrong!', 500);
+  }
 
   if (process.env.NODE_ENV === 'development') {
-    sendDevError(err, res);
+    sendDevError(error, res);
   } else if (process.env.NODE_ENV) {
-    sendProdError(err, res);
+    sendProdError(error, res);
   }
 };
 
